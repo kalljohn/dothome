e8eb2b63adb1e09884b7a217eb96ce632fef7179
Benson Lin <bensonlin@qnap.com>
2019-12-19 07:44:06 +0000

    update makefile

--- vimfiles/setup-plugins.sh
+++ vimfiles/setup-plugins.sh
@@ -4,6 +4,4 @@
 
 curl -fLo ./autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-([ ! -e ~/.vimrc ] || rm ~/.vimrc) && ln -s -f `pwd`/_vimrc ~/.vimrc
-
-sh -c "vim +PlugInstall +qall"
+vim -E -s -u ./_vimrc +PlugInstall +qall
